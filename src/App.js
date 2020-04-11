f4ce461a723787fddf1a57e131206fa180d0d245
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-11 17:42:56 +0200

    add setState to App, ToDoForm

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import ToDoForm from "./components/ToDoForm";
 import List from "./components/List";
 
 function App() {
+    const [todos, setTodos] = useState({lastId: 1, list: []});
+    function addTask (newId, todo) {
+        setTodos({lastId: newId, list: [todo, ...todos.list]})
+    }
   return (
     <div className="App">
         <div className="container">
-            <ToDoForm />
-            <List />
+            <ToDoForm lastId={todos.lastId} addTask={addTask}/>
+            <List list={todos.list}/>
         </div>
     </div>
   );
--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoForm(props) {
-    function validateForm(e) {
+    const [todo, setTodo] = useState({
+        id: 0,
+        item: '',
+        finished: false
+    });
+    function inputChange (e) {
+        setTodo({...todo, item: e.target.value, id: props.lastId});
+    }
+    function handleSubmitForm (e) {
         e.preventDefault();
-        let form = document.querySelector('.needs-validation'),
-            inputText = document.getElementById('todo');
-        if (inputText.value.trim() === ''){
+        const form = document.querySelector('.needs-validation'),
+              newTask = form.elements["newTask"].value;
+        if (!newTask.trim()){
             form.classList.add('was-validated');
+        } else {
+            const newId = todo.id + 1;
+            props.addTask(newId, todo);
+            setTodo({...todo, item: ''});
         }
     }
     return (
         <div className="row justify-content-center">
-            <form className="sol-sm-6 col-lg-5 mb-5 needs-validation" onSubmit={validateForm} action="" noValidate>
+            <form className="sol-sm-6 col-lg-5 mb-5 needs-validation" onSubmit={handleSubmitForm} action="" noValidate>
                 <div className="input-group input-group-lg">
-                    <input type="text" className="form-control" placeholder="Enter text ..." id="todo" required/>
+                    <input type="text"
+                           name="newTask"
+                           value={todo.item}
+                           className="form-control"
+                           placeholder="Enter text ..."
+                           id="todo" required
+                           onChange={inputChange}
+                    />
                     <div className="input-group-append">
                         <button type="submit" className="btn btn-primary">Add</button>
                     </div>
