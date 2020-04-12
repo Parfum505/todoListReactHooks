951e0422fe6bbe85facfa4878a40cb85fc360fb7
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-12 16:28:28 +0200

    add gh-pages

---
+++ precache-manifest.ccb3d758edb2753cc6688a9179310633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa4d6e90dfbcf2e22173cd8adc920e3",
+    "url": "/todoListReactHooks/index.html"
+  },
+  {
+    "revision": "6d5d16c9fe5235331d18",
+    "url": "/todoListReactHooks/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ed3f173b4d9caf4709f4",
+    "url": "/todoListReactHooks/static/css/main.76bb5ded.chunk.css"
+  },
+  {
+    "revision": "6d5d16c9fe5235331d18",
+    "url": "/todoListReactHooks/static/js/2.2c13e8e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoListReactHooks/static/js/2.2c13e8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3f173b4d9caf4709f4",
+    "url": "/todoListReactHooks/static/js/main.c177be3c.chunk.js"
+  },
+  {
+    "revision": "090325f87e61520ea813",
+    "url": "/todoListReactHooks/static/js/runtime-main.8a636790.js"
+  }
+]);
